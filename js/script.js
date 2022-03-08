b41a40aae9ddb75dba28db2ac386a14bba6495f2
Dzhalal <sparkereddd@gmail.com>
2022-03-08 21:19:13 +0300

    fx

--- js/script.js
+++ js/script.js
@@ -47,7 +47,7 @@ var rellax = new Rellax('.rellax');
 
 /* ************************** About slider ************************** */
 $('.about__slider').slick({
-  // speed: 1000,
+  swipe: false,
   waitForAnimate: false,
   prevArrow: `<button type="button" class="slider__arrow slider-prev about__slider__button about__slider__button-prev"><img src="images/white-arrow.svg" alt=""></button>`,
   nextArrow: `<button type="button"class="slider__arrow slider-next about__slider__button about__slider__button-next"><img src="images/white-arrow.svg" alt=""></button>`,
